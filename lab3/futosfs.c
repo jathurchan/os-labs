ee8ee4ae3eb293ee55cb514225a8bcfce0ded217
jathurchan <admin@jathurchan.com>
2022-12-11 22:26:04 +0100

    getattr

--- lab3/futosfs.c
+++ lab3/futosfs.c
@@ -79,39 +79,40 @@ void displayFilesystemInfo(void* mapPointer) {
 }
 
 
-static int hello_stat(fuse_ino_t ino, struct stat *stbuf)
+static int tosfs_stat(fuse_ino_t ino, struct stat *stbuf)
 {
-	stbuf->st_ino = ino;
-	switch (ino) {
-	case 1:
-		stbuf->st_mode = S_IFDIR | 0755;
-		stbuf->st_nlink = 2;
-		break;
-
-	case 2:
-		stbuf->st_mode = S_IFREG | 0444;
-		stbuf->st_nlink = 1;
-		stbuf->st_size = strlen(hello_str);
-		break;
-
-	default:
-		return -1;
+	struct tosfs_inode *in = inode_cache + ino;
+	assert(inode_cache != NULL);
+	if (ino == in->inode) {
+		if (in->inode != 0) {
+			stbuf->st_ino = ino;
+			stbuf->st_utd = in->uid;
+			stbuf->st_gid = in->gid;
+			stbuf->st_mode = in->mode;
+			stbuf->st_blocks = 1;
+			stbuf->st_size = in->size;
+			stbuf->st_nlink = in->nlink;
+			return 0;
+		} else {
+			return -ENDENT;
+		}
+	} else {
+		return -EIO;
 	}
-	return 0;
 }
 
-static void hello_ll_getattr(fuse_req_t req, fuse_ino_t ino,
+static void tosfs_ll_getattr(fuse_req_t req, fuse_ino_t ino,
 			     struct fuse_file_info *fi)
 {
 	struct stat stbuf;
-
-	(void) fi;
-
 	memset(&stbuf, 0, sizeof(stbuf));
-	if (hello_stat(ino, &stbuf) == -1)
-		fuse_reply_err(req, ENOENT);
+	
+	int ret = tosfs_stat(ino, &stbuf);
+
+	if (ret == 0)
+		fuse_reply_err(req, &stbuf, 1.0);
 	else
-		fuse_reply_attr(req, &stbuf, 1.0);
+		fuse_reply_attr(req, -ret);
 }
 
 static void hello_ll_lookup(fuse_req_t req, fuse_ino_t parent, const char *name)
