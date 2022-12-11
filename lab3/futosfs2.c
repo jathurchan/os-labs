c1b110446a292e71a7ca62a2da5d6d6c319c6169
Lor1ndel <flexi12.dauphin@gmail.com>
2022-12-11 22:11:08 +0100

    Lab 3 Q2

--- lab3/futosfs2.c
+++ lab3/futosfs2.c
@@ -1,5 +1,3 @@
-#define FUSE_USE_VERSION 26
-
 #include <sys/mman.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,7 +5,6 @@
 #include <fcntl.h>
 #include "tosfs.h"
 #include <linux/types.h>
-#include <fuse_lowlevel.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,9 +12,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <assert.h>
-
-static const char *hello_str = "Hello World!\n";
-static const char *hello_name = "hello";
+#include <fuse_lowlevel.h>
 
 const char* fileSystem = "test_tosfs_files";
 int fileSize;
@@ -78,137 +73,176 @@ void displayFilesystemInfo(void* mapPointer) {
                i, initDentry(mapPointer, i).inode, initDentry(mapPointer, i).name);
 }
 
+static int futosfs_stat(fuse_ino_t ino, struct stat *stbuf)
+{
+    stbuf->st_ino = ino;
+    switch (ino) {
+        case 1:
+            stbuf->st_mode = S_IFDIR | 0755;
+            stbuf->st_nlink = 2;
+            break;
+
+        case 2:
+            stbuf->st_mode = S_IFREG | 0444;
+            stbuf->st_nlink = 1;
+            stbuf->st_size = strlen(hello_str);
+            break;
+
+        default:
+            return -1;
+    }
+    return 0;
+}
+
+static void futosfs_ll_getattr(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi)
+{
+    struct stat stbuf;
+
+    (void) fi;
+
+    memset(&stbuf, 0, sizeof(stbuf));
+    if (hello_stat(ino, &stbuf) == -1)
+        fuse_reply_err(req, ENOENT);
+    else
+        fuse_reply_attr(req, &stbuf, 1.0);
+}
+
+
 
 static int hello_stat(fuse_ino_t ino, struct stat *stbuf)
 {
-	stbuf->st_ino = ino;
-	switch (ino) {
-	case 1:
-		stbuf->st_mode = S_IFDIR | 0755;
-		stbuf->st_nlink = 2;
-		break;
+    stbuf->st_ino = ino;
+    switch (ino) {
+        case 1:
+            stbuf->st_mode = S_IFDIR | 0755;
+            stbuf->st_nlink = 2;
+            break;
 
-	case 2:
-		stbuf->st_mode = S_IFREG | 0444;
-		stbuf->st_nlink = 1;
-		stbuf->st_size = strlen(hello_str);
-		break;
+        case 2:
+            stbuf->st_mode = S_IFREG | 0444;
+            stbuf->st_nlink = 1;
+            stbuf->st_size = strlen(hello_str);
+            break;
 
-	default:
-		return -1;
-	}
-	return 0;
+        default:
+            return -1;
+    }
+    return 0;
 }
 
 static void hello_ll_getattr(fuse_req_t req, fuse_ino_t ino,
-			     struct fuse_file_info *fi)
+                             struct fuse_file_info *fi)
 {
-	struct stat stbuf;
+    struct stat stbuf;
 
-	(void) fi;
+    (void) fi;
 
-	memset(&stbuf, 0, sizeof(stbuf));
-	if (hello_stat(ino, &stbuf) == -1)
-		fuse_reply_err(req, ENOENT);
-	else
-		fuse_reply_attr(req, &stbuf, 1.0);
+    memset(&stbuf, 0, sizeof(stbuf));
+    if (hello_stat(ino, &stbuf) == -1)
+        fuse_reply_err(req, ENOENT);
+    else
+        fuse_reply_attr(req, &stbuf, 1.0);
 }
 
 static void hello_ll_lookup(fuse_req_t req, fuse_ino_t parent, const char *name)
 {
-	struct fuse_entry_param e;
+    struct fuse_entry_param e;
 
-	if (parent != 1 || strcmp(name, hello_name) != 0)
-		fuse_reply_err(req, ENOENT);
-	else {
-		memset(&e, 0, sizeof(e));
-		e.ino = 2;
-		e.attr_timeout = 1.0;
-		e.entry_timeout = 1.0;
-		hello_stat(e.ino, &e.attr);
+    if (parent != 1 || strcmp(name, hello_name) != 0)
+        fuse_reply_err(req, ENOENT);
+    else {
+        memset(&e, 0, sizeof(e));
+        e.ino = 2;
+        e.attr_timeout = 1.0;
+        e.entry_timeout = 1.0;
+        hello_stat(e.ino, &e.attr);
 
-		fuse_reply_entry(req, &e);
-	}
+        fuse_reply_entry(req, &e);
+    }
 }
 
 struct dirbuf {
-	char *p;
-	size_t size;
+    char *p;
+    size_t size;
 };
 
 static void dirbuf_add(fuse_req_t req, struct dirbuf *b, const char *name,
-		       fuse_ino_t ino)
+                       fuse_ino_t ino)
 {
-	struct stat stbuf;
-	size_t oldsize = b->size;
-	b->size += fuse_add_direntry(req, NULL, 0, name, NULL, 0);
-	b->p = (char *) realloc(b->p, b->size);
-	memset(&stbuf, 0, sizeof(stbuf));
-	stbuf.st_ino = ino;
-	fuse_add_direntry(req, b->p + oldsize, b->size - oldsize, name, &stbuf,
-			  b->size);
+    struct stat stbuf;
+    size_t oldsize = b->size;
+    b->size += fuse_add_direntry(req, NULL, 0, name, NULL, 0);
+    b->p = (char *) realloc(b->p, b->size);
+    memset(&stbuf, 0, sizeof(stbuf));
+    stbuf.st_ino = ino;
+    fuse_add_direntry(req, b->p + oldsize, b->size - oldsize, name, &stbuf,
+                      b->size);
 }
 
 #define min(x, y) ((x) < (y) ? (x) : (y))
 
 static int reply_buf_limited(fuse_req_t req, const char *buf, size_t bufsize,
-			     off_t off, size_t maxsize)
+                             off_t off, size_t maxsize)
 {
-	if (off < bufsize)
-		return fuse_reply_buf(req, buf + off,
-				      min(bufsize - off, maxsize));
-	else
-		return fuse_reply_buf(req, NULL, 0);
+    if (off < bufsize)
+        return fuse_reply_buf(req, buf + off,
+                              min(bufsize - off, maxsize));
+    else
+        return fuse_reply_buf(req, NULL, 0);
 }
 
 static void hello_ll_readdir(fuse_req_t req, fuse_ino_t ino, size_t size,
-			     off_t off, struct fuse_file_info *fi)
+                             off_t off, struct fuse_file_info *fi)
 {
-	(void) fi;
+    (void) fi;
 
-	if (ino != 1)
-		fuse_reply_err(req, ENOTDIR);
-	else {
-		struct dirbuf b;
+    if (ino != 1)
+        fuse_reply_err(req, ENOTDIR);
+    else {
+        struct dirbuf b;
 
-		memset(&b, 0, sizeof(b));
-		dirbuf_add(req, &b, ".", 1);
-		dirbuf_add(req, &b, "..", 1);
-		dirbuf_add(req, &b, hello_name, 2);
-		reply_buf_limited(req, b.p, b.size, off, size);
-		free(b.p);
-	}
+        memset(&b, 0, sizeof(b));
+        dirbuf_add(req, &b, ".", 1);
+        dirbuf_add(req, &b, "..", 1);
+        dirbuf_add(req, &b, hello_name, 2);
+        reply_buf_limited(req, b.p, b.size, off, size);
+        free(b.p);
+    }
 }
 
 static void hello_ll_open(fuse_req_t req, fuse_ino_t ino,
-			  struct fuse_file_info *fi)
+                          struct fuse_file_info *fi)
 {
-	if (ino != 2)
-		fuse_reply_err(req, EISDIR);
-	else if ((fi->flags & 3) != O_RDONLY)
-		fuse_reply_err(req, EACCES);
-	else
-		fuse_reply_open(req, fi);
+    if (ino != 2)
+        fuse_reply_err(req, EISDIR);
+    else if ((fi->flags & 3) != O_RDONLY)
+        fuse_reply_err(req, EACCES);
+    else
+        fuse_reply_open(req, fi);
 }
 
 static void hello_ll_read(fuse_req_t req, fuse_ino_t ino, size_t size,
-			  off_t off, struct fuse_file_info *fi)
+                          off_t off, struct fuse_file_info *fi)
 {
-	(void) fi;
+    (void) fi;
 
-	assert(ino == 2);
-	reply_buf_limited(req, hello_str, strlen(hello_str), off, size);
+    assert(ino == 2);
+    reply_buf_limited(req, hello_str, strlen(hello_str), off, size);
 }
 
 static struct fuse_lowlevel_ops hello_ll_oper = {
-	//.lookup		= hello_ll_lookup,
-	//.getattr	= hello_ll_getattr,
-	//.readdir	= hello_ll_readdir,
-	//.open		= hello_ll_open,
-	//.read		= hello_ll_read,
+        .lookup		= hello_ll_lookup,
+        .getattr	= hello_ll_getattr,
+        .readdir	= hello_ll_readdir,
+        .open		= hello_ll_open,
+        .read		= hello_ll_read,
 };
 
-int startFuse(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    void* mapPointer = mapFileSystem();
+    displayFilesystemInfo(mapPointer);
+
     struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
     struct fuse_chan *ch;
     char *mountpoint;
@@ -234,8 +268,4 @@ int startFuse(int argc, char *argv[]) {
     fuse_opt_free_args(&args);
 
     return err ? 1 : 0;
-}
-
-int main(int argc, char *argv[]) {
-    startFuse(argc, argv);
 }
