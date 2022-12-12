f7925fed4dfa8d525cb79479e7306b3412c6ab56
Lor1ndel <flexi12.dauphin@gmail.com>
2022-12-12 11:35:47 +0100

    Lab 4

--- lab4/segclt.c
+++ lab4/segclt.c
@@ -1,6 +1,57 @@
 #include "segdef.h"
+#include <sys/types.h>
+#include <sys/sem.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/shm.h>
+#include <unistd.h>
+
+int semId, shmId, reqCount = 0, randSum = 0;
+char *buf;
+struct sembuf sop;
+struct shmseg seg;
+
+void initClt() {
+
+    if ((shmId = shmget(cle, 0, 0)) == -1)
+        exit(EXIT_FAILURE);
+
+    if ((buf = shmat(shmId, 0, 0)) == (char*)-1)
+        exit(EXIT_FAILURE);
+
+    if ((semId = semget(cle, 0, 0) == -1))
+        exit(EXIT_FAILURE);
+
+    init_rand();
+}
+
+void initSeg() {
+    seg.pid = getpid();
+    seg.req = reqCount++;
+    for (int i = 0; i < maxval; i++)
+        randSum += (seg.tab[i] = getrand());
+    seg.result = randSum/maxval;
+}
+
+void loopClt() {
+
+    acq_sem(semId, seg_dispo);
+
+    initSeg();
+    wait_sem(semId, res_ok);
+
+    printf("Result Client: %ld\n", seg.result);
+    lib_sem(semId, seg_init);
+
+    wait_sem(semId, res_ok);
+
+    lib_sem(semId, seg_dispo);
+
+    printf("Result Server: %ld\n", seg.result);
+}
 
 int main() {
-    init_rand();
-    return 0;
+    initClt();
+    //while(1)
+        loopClt();
 }
